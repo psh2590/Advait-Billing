a5aec3c6ab2c764af9d4b8136f28a3fc15d29492
psh2590 <piyush.ajpandey@gmail.com>
2026-02-08 12:44:04 +0530

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const paymentController = require('../controllers/paymentController');
 
+router.post('/generate-qr', paymentController.generateQRCode);
+router.post('/confirm', paymentController.confirmPayment);
+router.get('/history', paymentController.getPaymentHistory);
+router.get('/:payment_id', paymentController.checkPaymentStatus);
+
+module.exports = router;
