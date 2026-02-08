9ccfa96e77195c8cb05099c908139198ad9f37ac
psh2590 <piyush.ajpandey@gmail.com>
2026-02-08 12:43:08 +0530

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
 
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.post('/', productController.addProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+router.patch('/:id/stock', productController.updateStock);
+
+module.exports = router;
