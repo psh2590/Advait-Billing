574fd9035610625ebd5fd9b7529a11aa37f28eb5
psh2590 <piyush.ajpandey@gmail.com>
2026-02-08 12:43:35 +0530

    Update billRoutes.js

--- src/routes/billRoutes.js
+++ src/routes/billRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const billController = require('../controllers/billController');
 
+router.post('/', billController.createBill);
+router.get('/', billController.getAllBills);
+router.get('/daily-sales', billController.getDailySales);
+router.get('/:id', billController.getBillById);
+router.patch('/:id/status', billController.updatePaymentStatus);
+
+module.exports = router;
